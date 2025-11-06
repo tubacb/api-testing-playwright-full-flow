bcee54443d508454174b95db0c7d1e2583bb57a0
tuba bilgin <tubacobann@gmail.com>
2025-11-06 18:38:42 +0200

    add test

--- tests/helpers/api-helper.ts
+++ tests/helpers/api-helper.ts
@@ -50,3 +50,26 @@ export async function getOrderById(
     data.id,
   )
 }
+export async function deleteOrder(request: APIRequestContext, jwt: string, orderId:number): Promise<void> {
+  const response = await request.delete(`${serviceURL}${orderPath}/${orderId}`, {
+   headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  })
+  expect(response.status()).toBe(StatusCodes.OK)
+}
+export async function getDeletedOrderById(
+  request: APIRequestContext,
+  jwt: string,
+  id: number,
+): Promise<void> {
+  const response = await request.get(`${serviceURL}${orderPath}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  })
+  const data=await response.text()
+  expect(data).toBe('')
+
+
+}
